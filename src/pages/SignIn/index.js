b4ba87dde0041b0738fb83410167abdbf37afc47
JoelSantoss <joelzinvittore@gmail.com>
2024-01-24 09:23:59 -0300

    paginaDeLogin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,36 @@
+import './signIn.css';
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function SignIn(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
-        <div>
-            <h1>Pagina login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                  <img src={logo} alt='Logo de sistema de login'/>
+                </div>
+            <form>
+                <h1>Entrar</h1>
+                <input type='text'
+                 placeholder='email@email.com'
+                 value={email}
+                 onChange={ (e) => setEmail(e.target.value)}
+                 />
+
+                  <input type='password'
+                 placeholder='*******'
+                 value={password}
+                 onChange={ (e) => setPassword(e.target.value)}
+                 />
+
+                 <button type='submit'>Acessar</button>
+            </form>
+            <Link to="register"> Criar conta</Link>
+            </div>
         </div>
     )
 }
