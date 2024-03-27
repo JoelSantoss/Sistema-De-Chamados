5666408d9b10108d48c3895d81fb1d79a6215822
JoelSantoss <joelzinvittore@gmail.com>
2024-03-27 14:24:16 -0300

    atualizandoPerfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,12 @@ import { FiSettings, FiUpload } from 'react-icons/fi';
 import avatar from '../../assets/avatar.png';
 import { AuthContext } from '../../Contexts/auth';
 import { useContext, useState } from 'react';
+
+import {doc, updateDoc} from 'firebase/firestore';
+import {db, storage} from '../../Services/firebaseConnection';
+import {ref, uploadBytes, getDownloadURL} from 'firebase/storage';
+
+import { toast } from 'react-toastify';
 import "./profile.css"
 
 export default function Profile(){
@@ -30,6 +36,58 @@ export default function Profile(){
       }
     }
 
+    async function handleUpload(){
+         const currentUid = user.uid;
+
+         const uploadRef = ref(storage,`images/${currentUid}/${imageAvatar.name}`);
+         const uploadtask = uploadBytes(uploadRef, imageAvatar)
+         .then((snapshot)=>{
+           
+          getDownloadURL(snapshot.ref).then(async(downLoadURL)=>{
+               let urlFoto = downLoadURL;
+
+               const docRef = doc(db, 'users', user.uid )
+               await updateDoc(docRef, {
+                avatarUrl : urlFoto,
+                nome: nome
+
+               })
+               .then(()=>{
+                let data = {
+                  ...user,
+                  nome : nome,
+                  avatarUrl : urlFoto,
+                }
+                setUser(data);
+                storageUser(data);
+                toast.success("Atualizado com sucesso")
+               })
+          })
+         })
+    }
+
+    async function handleSubmit(e){
+      e.preventDefault();
+
+      if(imageAvatar == null && nome !== "" ){
+         const docRef = doc(db, 'users', user.uid)
+         await updateDoc(docRef, {
+          nome: nome
+         }).then(()=>{
+          let data = {
+            ...user,
+            nome : nome,
+          }
+          setUser(data);
+          storageUser(data);
+          toast.success("Atualizado com sucesso")
+         })
+      }
+      else if(nome !=='' && imageAvatar !== null){
+        handleUpload();
+      }
+    }
+
     return(
         <div>
             <Header/>
@@ -40,7 +98,7 @@ export default function Profile(){
 
                <div className='container'>
                  
-                 <form className='form-profile'>
+                 <form className='form-profile' onSubmit={handleSubmit}>
                    <label className='label-avatar'>
                      <span>
                         <FiUpload color='#FFF' size={25}/>
