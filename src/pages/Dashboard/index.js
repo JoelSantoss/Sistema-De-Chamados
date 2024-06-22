b0794b966db9834a32b471c649ab32a5ef222570
JoelSantoss <joelzinvittore@gmail.com>
2024-06-22 15:41:09 -0300

    dashboardPage

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,11 @@ import { AuthContext } from "../../Contexts/auth";
 import { useContext } from "react";
 
 import Header from "../../components/Header";
+import Title from "../../components/Title";
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit} from 'react-icons/fi';
+import { Link } from 'react-router-dom'
+
+import './dashboard.css';
 
 
 export default function Deashboard(){
@@ -16,8 +21,48 @@ export default function Deashboard(){
         <div>
 
             <Header/>
-         <h1> Pagina Deashboard</h1>
-         <button onClick={handleLogout} >Sair</button>
+             
+             <div className="content">
+                <Title name='Chamados'>
+                    <FiMessageSquare size={25}/>
+                </Title>
+
+                 <>
+                  <Link className="new" to="/new">
+                    <FiPlus color="#FFF" size={25}/>
+                    Novo Chamado
+                  </Link>
+                
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope="col">Cliente</th>
+                            <th scope="col">Assunto</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Cadastrado em:</th>
+                            <th scope="col">#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-label="Cliente">Mercado esquina</td>
+                            <td data-label="Assunto">Suporte</td>
+                            <td data-label="Status">Em aberto</td>
+                            <td data-label="Cadastrado">12/05/2024</td>
+                            <td data-label="#">
+                                <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                    <FiSearch color="#FFF" size={17}/>
+                                </button>
+                                <button className="action" style={{backgroundColor: '#f6a935'}}>
+                                    <FiEdit color="#FFF" size={17}/>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </>
+              
+             </div>
         </div>
     )
 }
